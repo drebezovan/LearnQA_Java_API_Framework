d193410c9dff70d88e557881424cb35a61af01e4
drebezovan <drebezovan@mail.ru>
2023-09-24 02:49:39 +0300

    Создала тест UserRegisterTest и добавила 2 проверки в Assertions

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -13,4 +13,12 @@ public class Assertions {
         int value = Response.jsonPath().getInt(name);
         assertEquals(expectedValue, value, "Значение из Json не совпадает с ожидаемым значением");
     }
+
+    public static void assertResponseTextEquals(Response Response, String expectedAnswer){
+        assertEquals(expectedAnswer, Response.asString(), "Текст ответа не совпадает с ожидаемым значением");
+    }
+
+    public static void assertResponseCodeEquals(Response Response, int expectedStatusCode){
+        assertEquals(expectedStatusCode, Response.getStatusCode(), "Статус код ответа не совпадает с ожидаемым значением");
+    }
 }
