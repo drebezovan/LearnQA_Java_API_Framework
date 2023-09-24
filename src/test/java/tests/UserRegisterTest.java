86c25cab978cb84e787f47fd49bb91d1b62c5de7
drebezovan <drebezovan@mail.ru>
2023-09-24 03:46:54 +0300

    Добавила проверку нескольких полей assertJsonHasFields, написала тест testGetUserDetailsAuthAsSameUser и отрефакторила названия проверок assertJsonHasField и assertJsonHasNotField

--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -53,6 +53,6 @@ public class UserRegisterTest extends BaseTestCase {
                 .andReturn();
 
         Assertions.assertResponseCodeEquals(responseCreateAuth, 200);
-        Assertions.assertJsonHasKey(responseCreateAuth, "id");
+        Assertions.assertJsonHasField(responseCreateAuth, "id");
     }
 }
